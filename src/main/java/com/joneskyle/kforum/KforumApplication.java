9ba55d8828b2f818884a030e11f05302d6ab6576
Kyle Jones <mynameiskyle242@gmail.com>
2020-02-15 18:15:28 -0500

    Initial commit

---
+++ src/main/java/com/joneskyle/kforum/KforumApplication.java
@@ -0,0 +1,13 @@
+package com.joneskyle.kforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KforumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joneskyle/kforum/KforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joneskyle.kforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KforumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
